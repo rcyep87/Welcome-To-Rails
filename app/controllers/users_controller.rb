28df3f494083ae31d5274fbcf67096e4818bae22
Ryan Yep <ryan.yep@gmail.com>
2015-09-15 17:23:41 -0500

    Displays ALL users from the db where first_name starts with s.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,17 @@ class UsersController < ApplicationController
 
   def index
     user = User.all
-      user_array = user.map do |x|
-        "Name: #{x.first_name} #{x.last_name}, Age: #{x.age}"
-      end
-    render text: "#{user_array}"
+      if params["first_name"] == "s"
+        user_array = user.where("first_name LIKE ?", "s%").map do |x|
+          "Name: #{x.first_name} #{x.last_name}, Age: #{x.age}"
+        end
+        render text: "#{user_array}"
+      else
+        user_array = user.map do |x|
+          "Name: #{x.first_name} #{x.last_name}, Age: #{x.age}"
+        end
+      render text: "#{user_array}"
+    end
   end
 
   def show
@@ -16,5 +23,7 @@ class UsersController < ApplicationController
       render text: "Sorry, user with id of #{params[:id]} was not found", status: 404
     end
   end
+
+  # User.where("first_name LIKE ?", "s%")
 
 end
