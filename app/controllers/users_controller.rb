c18888532fff4bb3cee45d63115eaeac343f5f71
Ryan Yep <ryan.yep@gmail.com>
2015-09-15 16:26:55 -0500

    Requires action show for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,4 @@ class UsersController < ApplicationController
     render text: "#{user_array}"
   end
 
-  def show
-    begin
-      user = User.find(params[:id])
-      render text: "params: #{}"
-  end
-
 end
