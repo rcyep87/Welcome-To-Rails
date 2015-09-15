28f06a4565f30958cdb3f8a3d8533757dbee4ac3
Ryan Yep <ryan.yep@gmail.com>
2015-09-15 16:15:32 -0500

    Prints out all users from the database as plain text.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def index
-    user_array = []
-    User.all.each do |user|
-      user_array << user.first_name
+    user = User.all
+      user_array = user.map do |x|
+        "Name: #{x.first_name} #{x.last_name}, Age: #{x.age}"
       end
     render text: "#{user_array}"
   end
