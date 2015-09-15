9e22069e67f139e29315d1a00a77608ff6f859a5
Ryan Yep <ryan.yep@gmail.com>
2015-09-15 15:45:13 -0500

    successfully able to print user's first names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
-class UsersController < ApplicationsController
+class UsersController < ApplicationController
 
   def index
+    user_array = []
+    User.all.each do |user|
+      user_array << user.first_name
+      end
+    render text: "#{user_array}"
   end
 
 end
